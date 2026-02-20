1fe4f37110ebccbce61a3aa871a02d747f1b4755
mittalnikhil149 <mittalnikhil844@gmail.com>
2026-02-20 17:11:24 +0530

    save

---
+++ Multipl.java
@@ -0,0 +1,11 @@
+class Multi {
+    void getMulti(int a,int b){
+        System.out.println((a+b));
+    }
+}
+public class Multipl{
+    public static void main(String[]args){
+        Multi mu=new Multi();
+        mu.getMulti(15,10);
+            }
+}
--- calc/Calculator.java
+++ calc/Calculator.java
@@ -2,14 +2,27 @@ package calc;
 
 class sum{
     int num1,num2;
+    sum(){          //user define default constructor
+        this(10,10);
+
+    }
+    sum(int num1,int num2){
+        this.num1=num1;
+        this.num2=num2;
+        
+    }
     void getSum(){
         System.out.println("sum ="+(num1+num2));
     }
 }
 public class Calculator{
     public static void main(String[]args){
-        sum s=new sum();
+        sum s;   //object decleration
+        s=new sum();    //object instantiation
         s.getSum();
+        // sum s2;
+        // s2.getSum();
+        // new sum(15,15).getSum(); //object creation without reference variable
         
     }
 }
