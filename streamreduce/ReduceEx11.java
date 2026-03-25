259b69b071b2cf011e0712d10b10edc318b6158d
mittalnikhil149 <mittalnikhil844@gmail.com>
2026-03-25 16:35:11 +0530

    commit

--- streamreduce/ReduceEx11.java
+++ streamreduce/ReduceEx11.java
@@ -1,12 +1,12 @@
-package streamreduce;
+// package streamreduce;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class ReduceEx6 {
+public class ReduceEx11 {
      public static void main(String[]args){
         List<Integer>list=Arrays.asList(12,23,34,45,45,34,23);
-        long result=list.stream().reduce(0,(a,b)-> a%2==0 ? a+b:b);
+        long result=list.stream().reduce(0,(a,b)->(a+b)/list.size());
         System.out.println(result);
     }
     
